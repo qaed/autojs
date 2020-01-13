f8edc533b5937494ac449a23f3ce5234f52e0dd0
sheng <1103277858@qq.com>
2020-01-13 20:51:18 +0800

    first

---
+++ 东方头条.js
@@ -0,0 +1,12 @@
+//进入文章内执行
+while(true){
+    swipe(device.width / 2, device.height / 4 * 3,
+        device.width / 2, device.height / 2, 1000);
+    sleep(1000);
+    if(id("ach").findOnce()){
+        log("find-->"+id("ach").findOnce())
+        q = id("ach").findOnce().bounds();
+        click(q.centerX(), q.centerY());
+        sleep(5000);
+    }
+}
