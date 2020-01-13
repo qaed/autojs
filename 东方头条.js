1f613ca1ec4e915dab0ea1f67639db1483dee5f0
tsheay <1103277858@qq.com>
2020-01-13 21:24:34 +0800

    1

--- 东方头条.js
+++ 东方头条.js
@@ -4,7 +4,7 @@ while(true){
     swipe(device.width / 2, device.height / 4 * 3,
         device.width / 2, device.height / 2, 1000);
     sleep(1000);
-    if(id("ach").findOnce()){ //不包含广告
+    if(id("ach").findOnce()){//不包含广告
         //log("find-->"+id("ach").findOnce())
         q = id("ach").findOnce().bounds();
         click(q.centerX(), q.centerY());
